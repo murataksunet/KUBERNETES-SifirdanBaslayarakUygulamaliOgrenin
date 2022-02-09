69f40ceaae0414654d53e0284fb9020761a26ff3
murataksunet <65829321+murataksunet@users.noreply.github.com>
2022-02-09 20:10:56 +0300

    Add files via upload

---
+++ Examples/FILES/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Murat AKSU Kubernetes</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="css/styles.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:400,700" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div class="jumbotron">
+   <div class="container">
+            <h1 style="background-color:Tomato;" align="center">KUBERNETES: SIFIRDAN TUM YÖNLERIYLE </h1>
+          <p align="center">MURAT AKSU </p>
+<div align="left"><p>Adı:<input class="form-control" 
+        id="myTextBox1" 
+        name="myTextBox1" 
+        type="text" /></div>
+<div align="left"><p>Soyadı:<input class="form-control" 
+        id="myTextBox2" 
+        name="myTextBox2" 
+        type="text" /></div>
+<div align="left"><p>E-Posta:<input class="form-control" 
+        id="myTextBox3" 
+        name="myTextBox3" 
+        type="text" /></div>
+<div align="left"><p>Adres:<input class="form-control" 
+        id="myTextBox4" 
+        name="myTextBox4" 
+        type="text" /></div>
+<div align="left"><p>Meslek:<input class="form-control" 
+        id="myTextBox5" 
+        name="myTextBox5" 
+        type="text" /></div>
+<div align="left"><p>Görüşünüz<input class="form-control" 
+        id="myTextBox6" 
+        name="myTextBox6" 
+        type="text" /></div>
+<br>
+<p align="center"><a class="btn btn-primary btn-lg" href="http://www.murataksu.net" role="button">Gönder</a></p>
+
+        </div>
+    </div>
+</body>
+
+</html>
